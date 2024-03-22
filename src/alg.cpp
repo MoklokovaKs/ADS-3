17cdd57d09e65842d3a99cdaf4520a418e6dd0f7
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-03-22 23:14:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
+  //  поместить сюда реализацию алгоритма
     int count = 0;
     int left = 0;
     int right = size - 1;
@@ -28,5 +28,5 @@ int cbinsearch(int *arr, int size, int value) {
         }
     }
     return 0;
-  return 0; 
+  return 0; // если ничего не найдено
 }
